c792201424162813fbd0591179da4ab4575f6abb
tugbaustundag <tugbaustundag@outlook.com.tr>
2021-12-19 17:30:35 +0300

    day 12/2

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,16 +23,14 @@ namespace Business.Concrete
             
         }
 
+        
         public IResult Add(Car car)
         {
-            if (car.Description.Length < 2 )
-            {
-               return new ErrorResult(Messages.CarNameInvalid);
-            }
-            if (car.DailyPrice<=0)
-            {
-                return new ErrorResult(Messages.CarDailyPriceInvalid);
-            }
+            
+
+            ValidationTool.Validate(new CarValidator(),car);
+
+
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
